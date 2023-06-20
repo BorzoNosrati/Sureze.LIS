c6577bf87afc886b89606fc71f45769a1b76621b
BorzoNosrati <Borzo.nosrati@outlook.com>
2023-06-20 19:10:50 +0430

    Create Register Patient

--- Sureze.LIS.Application.Contracts2/Permissions/LISPermissions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sureze.LIS.Permissions;
-
-public static class LISPermissions
-{
-    public const string GroupName = "LIS";
-
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
-}
